248cbd27de95ab5c8569df57480afead5fe697e9
anees05 <anees.adams05@gmail.com>
2019-07-06 08:54:02 -0400

    Pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Pseudocode
+// 1. Create variables
+// 2. Create a timer that starts when question is asked and counts down until 0 until it resets for the next question
+// 3. Create array of questions and answers
+// 4. Create a way for the questions / answers to be randomly displayed 
+// 5. If user guess is correct then add a windo / if user guess is incorrect then add a loss
+// 6. If user guess is correct then display winning message / if user guess is incorrect then display losing message
+// 7. Display number of correct and incorrect answers
+// 8. Create a reset when game is over 
+
