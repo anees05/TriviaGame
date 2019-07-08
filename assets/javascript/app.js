98a9fd8895761fae57e7b6b93dd1179580311f6a
anees05 <anees.adams05@gmail.com>
2019-07-07 20:32:58 -0400

    Creating questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,69 @@
 // 7. Display number of correct and incorrect answers
 // 8. Create a reset when game is over 
 
+var wins = 0;
+var loss = 0;
+var timer = 10;
+var questionNum = 0;
+
+var questions = [
+    {
+        question: "How many teams competed in the 2018 FIFA World Cup?",
+        choices: ["32", "34", "30", "28"],
+        correctAnswer: "32"
+    },
+
+    {
+        question: "Who won the 1994 FIFA World Cup?",
+        choices: ["Argentina", "Germany", "Italy", "Brazil"],
+        correctAnswer: "Brazil"
+    },
+
+    {
+        question: "Which city has the most clubs competing in its country's top division",
+        choices: ["Rome", "Moscow", "Milan", "London"],
+        correctAnswer: "London"
+    },
+    
+    {
+        question: "How many MLS teams are based in Canada?",
+        choices: ["One", "Six", "Four", "Three"],
+        correctAnswer: "Three"
+    },
+
+    {
+        question: "Which club has won the most Serie A titles?",
+        choices: ["Internazional", "Lazio", "Milan", "Juventus"],
+        correctAnswer: "Juventus"
+    },
+
+    {
+        question: "Who are the only international team to win back-to-back European Championships?",
+        choices: ["Spain", "Italy", "France", "Germany"],
+        correctAnswer: "Spain"
+    },
+
+    {
+        question: "Which team won the 2015 Women's World Cup?",
+        choices: ["Japan", "England", "USA", "Canada"],
+        correctAnswer: "USA"
+    },
+
+    {
+        question: "Who won the first ever FA Premiere League?",
+        choices: ["Liverpool", "West Ham", "Arsenal", "Manchester United"],
+        correctAnswer: "Manchester United"
+    },
+
+    {
+        question: "Which player has scored the most number of goals at the FIFA World Cup?",
+        choices: [" Christiano Ronaldo", "Mess", "Zinedine Zidane", "Miroslav Klose"],
+        correctAnswer: "Miroslav Klose"
+    },
+
+    {
+        question: "Which is the only host country to get eliminated in the first round ",
+        choices: ["South Africa", "USA", "Canada", "Turkey"],
+        correctAnswer: "South Africa"
+    }
+]
